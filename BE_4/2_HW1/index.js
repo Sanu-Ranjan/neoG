8279d555120926313e51b916904f8b4142d3a979
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-03-26 18:53:05 +0530

    BE 4.2 HW1 : post routes to add restaurants

---
+++ BE_4/2_HW1/index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const { Restaurant } = require("../../BE_1_2/2.1_HW1/models/restaurants");
+const { initializeDB } = require("../../BE_1_2/2.1_CW/db/db.connect");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const dataObj = {
+  data: (data) => ({ data: data, error: null }),
+  error: (error) => ({ data: null, error: error }),
+};
+
+const success = (message, data) => {
+  return {
+    success: true,
+    message: message,
+    data: data,
+  };
+};
+
+const fail = (message, details = null) => {
+  return {
+    success: false,
+    error: message,
+    details: details,
+  };
+};
+
+const createRes = async (restaurantData) => {
+  try {
+    const data = await Restaurant.create(restaurantData);
+    return dataObj.data(data);
+  } catch (error) {
+    return dataObj.error(error);
+  }
+};
+
+app.post("/restaurants", async (req, res) => {
+  const restaurantData = req.body;
+
+  const { data, error } = await createRes(restaurantData);
+
+  if (error) {
+    console.log("Db error: error creating restaurant", error);
+    return res.status(500).json(fail("Internal server error", error.message));
+  }
+
+  res.status(201).json(success("Restaurant created", data));
+});
+
+(async () => {
+  await initializeDB();
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log("Server Listening on port :", PORT);
+  });
+})();
