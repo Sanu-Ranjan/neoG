7dabaf42eeebb0d2cd75c9e6514fe745b261b489
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-07-12 12:24:37 +0530

    "add position and auto close time for toast notifiactions"

--- MCR_BI/Frontend/rojgar-frontend/src/main.jsx
+++ MCR_BI/Frontend/rojgar-frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
   <>
     <App />
-    <ToastContainer />
+    <ToastContainer position="top-right" autoClose={2000} />
   </>,
 );
