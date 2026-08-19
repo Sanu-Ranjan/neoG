1568f6047c666ca93a0379b860836f4ff1c22a46
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-08-19 14:39:42 +0530

    json to csv

---
+++ FSP+/json to csv/jsonToCsv.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const { Parser } = require("json2csv");
+
+const jsonFilePath = path.join(__dirname, "data.json");
+
+const csvFilePath = path.join(__dirname, "data.csv");
+
+try {
+  const jsonData = fs.readFileSync(jsonFilePath, "utf-8");
+  const data = JSON.parse(jsonData);
+  console.log(data);
+  const json2csv = new Parser();
+  const csv = json2csv.parse(data);
+  fs.writeFileSync(csvFilePath, csv);
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  fs.writeFileSync;
+} catch (error) {}
